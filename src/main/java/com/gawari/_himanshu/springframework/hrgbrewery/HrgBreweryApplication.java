c226b88fdb3c1016a86c24a694135bc9fc399c6d
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-20 21:39:06 +0530

    initial commit

---
+++ src/main/java/com/gawari/_himanshu/springframework/hrgbrewery/HrgBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.gawari._himanshu.springframework.hrgbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrgBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gawari/_himanshu/springframework/hrgbrewery/HrgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gawari._himanshu.springframework.hrgbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrgBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
